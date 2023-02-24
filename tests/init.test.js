2177791953899ca42993bc1322884528ea16b1a8
georgegoun <georgiosgounaris@gmail.com>
2023-02-24 03:23:11 +0100

    Feature: Tests added

--- tests/init.test.js
+++ tests/init.test.js
@@ -49,6 +49,7 @@ test("GET /sources returns correct response and status code", async (t) => {
   t.is(statusCode, 200);
 });
 
+
 test("GET /test-url with valid URL returns expected response", async (t) => {
   const response = await t.context.got(
     "general/test-url?url=https://www.google.com",
@@ -71,7 +72,7 @@ test("GET /test-url with invalid URL returns expected response", async (t) => {
 });
 
 
-test("GET /dashboard returns the correct dashboard and sources", async (t) => {
+test("GET /dashboards returns the correct dashboard and sources", async (t) => {
   const token = jwtSign({ id: 1 });
   const response = await t.context.got(`dashboards/dashboards?token=${token}`);
 
@@ -95,9 +96,20 @@ test("GET /dashboard returns 403 as there unauthorized entry try", async (t) => 
 
 test("POST create and delete /dashboards", async (t) => {
   const token = jwtSign({ id: "638bb43acb0182b0c398149c" });
+
+  // Before creating try get dashboard -> should return 409 (no found)
+
+  const response_before = await t.context.got(`dashboards/dashboard?token=${token}`);
+  t.is(response_before.statusCode, 200);
+  t.is(response_before.body.status, 409);
+
+  // Create dashboard
+
+  let random = (Math.random() + 2).toString(36).substring(7);
+
   var options = {
     json: {
-      name: "new_dash",
+      name: random,
     },
     responseType: "json",
   };
@@ -106,19 +118,46 @@ test("POST create and delete /dashboards", async (t) => {
   t.is(response.statusCode, 200);
   t.is(response.body.success, true);
 
-  //console.log(response.body);
+  // Get dashboards
+
+  const response_view_dashboards = await t.context.got(`dashboards/dashboards?token=${token}`);
+  t.is(response_view_dashboards.statusCode, 200);
+  t.is(response_view_dashboards.body.success, true);
+  t.truthy(response_view_dashboards.body.dashboards);
+  
+  // Helper: Delete dashboard 
+  // let option ={
+  //   json: {
+  //     id: '',
+  //   },
+  //   responseType: "json",
+  // };
+  // const resp_delete = await t.context.got.post(`dashboards/delete-dashboard?token=${token}`, option);
+  // console.log(resp_delete.body);
+
+  // Export dashboard
+
+  const dashboard_id = response_view_dashboards.body.dashboards[0].id;
+  const response_export_dashboard = await t.context.got(`dashboards/dashboard?id=${dashboard_id}`,{
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  t.is(response_export_dashboard.statusCode, 200);
+  t.is(response_export_dashboard.body.success, true);
+  t.truthy(response_export_dashboard.body.dashboard);
+
+  // Delete dashboard
+
   options ={
     json: {
       id: response.body.id,
     },
     responseType: "json",
   };
-  
 
   const response_delete = await t.context.got.post(`dashboards/delete-dashboard?token=${token}`, options);
-  t.is(response.statusCode, 200);
+  t.is(response_delete.statusCode, 200);
 
-// const response = await t.context.got.post(`dashboards/check-password-needed?token=${token}`, options);
-  // t.is(response.statusCode, 200);
-  // t.is(response.body.success, true
+
 });
